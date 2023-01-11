2ab1d7543d614af6394b97866ea1263ca2f59dd1
danipetro21 <idany346@gmail.com>
2023-01-11 12:56:20 +0100

    MILESTONE 1

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +8,46 @@
   <link rel="stylesheet" href="css/style.css">
   <title>Password_generator</title>
 </head>
+
 <body>
-  
+
+  <h1>STRONG PASSWORD GENERATOR</h1>
+  <h2>Genera una password sicura</h2>
+
+
+  <form method="POST">
+    <label for="lpass">Lunghezza Password</label>
+    <input type="text" name="pass">
+    <input type="submit" value="GENERA">
+  </form>
+
+  <h1>
+
+    <?php
+    $lunghezza = $_POST['pass'];
+
+    function generaPass($lunghezza)
+    {
+      $caratteriPossibli = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+      $stringa = "";
+      $i = 0;
+      while ($i < $lunghezza) {
+        $carattere = substr($caratteriPossibli, rand(0, strlen($caratteriPossibli) - 1), 1);
+        if (!strstr($stringa, $carattere)) {
+          $stringa .= $carattere;
+          $i++;
+        }
+      }
+      return $stringa;
+    }
+
+    echo generaPass($lunghezza);
+    ?>
+
+  </h1>
+
+
+
 </body>
+
 </html>
